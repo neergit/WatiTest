8f7482f5244f18f699d91fd026d681d2d799f51a
Neeraj Chandra <neerajchandra12@gmail.com>
2023-09-14 15:53:06 +0530

    first commit
    httlp post with input two nums and returning their sum

---
+++ WATIAPITest/WATIAPITest/Controllers/AddController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WATIAPITest.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class AddController : ControllerBase
+    {
+		[HttpPost]
+		public ActionResult AddNumber(InputModel input)
+		{
+			if(input.num1 == 0 && input.num2 == 0)
+			{
+				return BadRequest();
+			}
+			int sum = input.num1 + input.num2;
+			return Ok(sum);
+		}
+	}
+
+	public class InputModel
+	{
+		public int num1 { get; set; }
+        public int num2 { get; set; }
+    }
+}
+
